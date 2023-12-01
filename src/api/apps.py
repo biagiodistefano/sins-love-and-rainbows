c27f78a7159af29eb10a0d4105292f365db50585
Biagio Distefano <biagiodistefano92@gmail.com>
2023-12-01 08:50:40 +0100

    fixed deployment

--- src/api/apps.py
+++ src/api/apps.py
@@ -1,13 +1,6 @@
 from django.apps import AppConfig
-from django.conf import settings
-from datetime import datetime
 
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-
-    def ready(self):
-        deploy_filepath = settings.BASE_DIR / 'deployments.txt'
-        with deploy_filepath.open("a") as f:
-            f.write(f"SLR Started on {datetime.now()}\n")
