924edceabc59685669f03e7f0e7d84310f8ccef4
Biagio Distefano <biagiodistefano92@gmail.com>
2025-01-20 09:12:15 +0100

    add phone number to list_display

--- src/api/admin.py
+++ src/api/admin.py
@@ -21,7 +21,7 @@ class PersonAdmin(admin.ModelAdmin):
 
     inlines = [AllergyInline]
 
-    list_display = ("first_name", "in_broadcast", "from_abroad", "whatsapp_message", "allergies_list")
+    list_display = ("first_name", "in_broadcast", "from_abroad", "phone_number", "whatsapp_message", "allergies_list")
 
     @admin.display(description="Message")
     def whatsapp_message(self, obj: Person) -> str:
